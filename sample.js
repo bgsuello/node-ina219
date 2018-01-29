f86fe4c839d66c6563c8b568841b9d4ae92ed623
bgsuello <bg.suello@gmail.com>
2018-01-29 17:31:12 +0800

    updated sample.

--- sample.js
+++ sample.js
@@ -1,16 +1,21 @@
 var ina219 = require('./ina219');
 
-  ina219.init(0x40, '3');
-  ina219.enableLogging(true);
-  
-  ina219.calibrate32V1A(function () {
-    
-    ina219.getBusVoltage_V(function (volts) {
-	  
-      console.log("Voltage: " + volts);
-      ina219.getCurrent_mA(function (current){
-  			
-        console.log("Current (mA): " + current );
-      });	
-    });
-  });
+const MAXBATTERYVOLTAGE = 4.2;
+const INA219_I2C_ADDRESS = 0x40;
+const INA219_I2C_BUS = '3';
+
+let getBatteryPercentage = function (batteryVoltage) {
+  return (batteryVoltage / MAXBATTERYVOLTAGE) * 100;
+}
+
+ina219.init(INA219_I2C_ADDRESS, INA219_I2C_BUS);
+ina219.enableLogging(false); // enable verbose logs
+
+// ina219.calibrate32V1A(function () {
+ina219.getBusVoltage_V(function (volts) {
+  console.log("Voltage: " + volts, ` --  ${getBatteryPercentage(volts)}`);
+});
+ina219.getCurrent_mA(function (current) {
+  console.log("Current (mA): " + current);
+});
+// });
